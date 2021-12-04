5247e0f60e42e7127a7b993ada29d6477ca342ea
chandlerHall47 <86384101+chandlerHall47@users.noreply.github.com>
2021-12-04 01:39:13 -0500

    Started GUI over with swing instead of scene builder

--- CisFlightApp/src/module-info.java
+++ CisFlightApp/src/module-info.java
@@ -1,6 +1,10 @@
 module CisFlightApp {
 	requires javafx.controls;
 	requires java.sql;
+	requires javafx.fxml;
+	requires javafx.base;
+	requires javafx.graphics;
+	requires java.desktop;
 	
 	opens edu.gsu.gui to javafx.graphics, javafx.fxml;
 }
